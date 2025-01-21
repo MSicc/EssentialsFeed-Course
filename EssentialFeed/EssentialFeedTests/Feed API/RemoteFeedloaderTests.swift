b7082dabfc36011ec144fc5c315a050e39ec66b2
Marco Siccardi <msiccdev@hotmail.com>
2025-01-21 21:44:55 +0100

    simplify type declarations with a helper function

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedloaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedloaderTests.swift
@@ -40,7 +40,7 @@ class RemoteFeedloaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity), when: {
+        expect(sut, toCompleteWith: failure(.connectivity), when: {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
         })
@@ -52,7 +52,7 @@ class RemoteFeedloaderTests: XCTestCase {
         let samples = [199,201,300,400,500]
         
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+            expect(sut, toCompleteWith: failure(.invalidData), when: {
                 let json = makeItemJson([])
                 client.complete(withStatusCode: code, data:json, at: index)
             })
@@ -62,7 +62,7 @@ class RemoteFeedloaderTests: XCTestCase {
     func test_load_deliversErrorOn200HTtpResponseWithInvalidJson() {
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+        expect(sut, toCompleteWith: failure(.invalidData), when: {
             let invalidJson = Data("Invalid JSON".utf8)
             client.complete(withStatusCode: 200, data: invalidJson)
         })
@@ -124,6 +124,10 @@ class RemoteFeedloaderTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(client, file: file, line: line)
         return (sut, client)
+    }
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
     
     private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
