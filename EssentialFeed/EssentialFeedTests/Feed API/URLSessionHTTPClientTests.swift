6d254115daa9382e27b1ed53a2d146fbeaa9873f
Marco Siccardi <msiccdev@hotmail.com>
2025-01-28 19:20:18 +0100

    move url creation to a shared factory method

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -38,9 +38,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     
     func test_getFromURL_performsGETRequestWithURL() {
-
-        
-        let url = URL(string: "http://any-url.com")!
+        let url = anyURL()
         let exp = expectation(description: "Wait for request")
         
         URLProtocolStub.observeRequests { request in
@@ -53,19 +51,16 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
         
-
     }
     
     
     func test_getFromURL_failsOnRequestError() {
-
-        let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data:nil, response: nil, error: error)
                 
         let exp = expectation(description: "Wait for completion")
         
-        makeSUT().get(from: url) { result in
+        makeSUT().get(from: anyURL()) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -89,7 +84,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         return sut
     }
     
-
+    private func anyURL() -> URL {
+        return URL(string: "https:/any-url.com")!
+    }
     
     private class URLProtocolStub: URLProtocol {
         private static var stub : Stub?
