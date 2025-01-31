a51f9a843c0d21add9f1456f1f012fc454a3a724
Marco Siccardi <msiccdev@hotmail.com>
2025-01-31 18:19:28 +0100

    Add end-to-end API tests in a separate target

---
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,16 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by MSiccDev on 31.01.2025.
+//
+
+import Testing
+
+struct EssentialFeedAPIEndToEndTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
