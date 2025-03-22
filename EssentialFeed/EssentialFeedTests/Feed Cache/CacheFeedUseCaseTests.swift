02c991ed4a3f6260edbe7b5c8949ceb673b66008
Marco Siccardi <msiccdev@hotmail.com>
2025-03-22 20:09:28 +0100

    save command does not request cache insertion on deletion error

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -22,14 +22,19 @@ class LocalFeedLoader{
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    var insertCallCount = 0
     
     func deleteCachedFeed() {
         deleteCachedFeedCallCount += 1
     }
+    
+    func completeDeletion(with error: Error, at index: Int = 0){
+        
+    }
 }
 
 class CacheFeedUseCaseTests : XCTestCase {
-
+    
     func test_init_doesNotDeleteCacheUponCreation() {
         let (_, store) = makeSUT()
         
@@ -38,16 +43,24 @@ class CacheFeedUseCaseTests : XCTestCase {
     
     
     func test_save_requestsCscheDeletion() {
-
-        let (sut, store) = makeSUT()
         let items = [uniqueItem(), uniqueItem()]
-
+        let (sut, store) = makeSUT()
         
         sut.save(items)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
     }
     
+    func test_save_doesNotRequestCacheInsertionInDeletionError() {
+        let items = [uniqueItem(), uniqueItem()]
+        let (sut, store) = makeSUT()
+        let deletionError = anyNSError()
+        
+        sut.save(items)
+        store.completeDeletion(with: deletionError)
+        
+        XCTAssertEqual(store.insertCallCount, 0)
+    }
     
     
     //MARK: - Helpers
@@ -67,4 +80,9 @@ class CacheFeedUseCaseTests : XCTestCase {
     private func anyURL() -> URL {
         return URL(string: "http://any-url.com")!
     }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
+    }
+    
 }
