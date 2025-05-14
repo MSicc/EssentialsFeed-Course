7c469934c5375b35d8f79f0e940e69cdb5a81ebe
Marco Siccardi <msiccdev@hotmail.com>
2025-05-14 18:45:20 +0200

    create empty project for prototype

---
+++ Prototype/Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Marco Siccardi on 14.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
