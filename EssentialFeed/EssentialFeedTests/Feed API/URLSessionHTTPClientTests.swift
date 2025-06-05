933fdf14fd8d0f1374fc0edcff37f4fa85ab066a
Marco Siccardi <msiccdev@hotmail.com>
2025-06-05 21:41:12 +0200

    increase timeout to accomodate for slow CI runs on Github

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -35,7 +35,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         makeSUT().get(from: url) { _ in }
         
-        wait(for: [exp], timeout: 1.0)
+        wait(for: [exp], timeout: 5.0)
         
     }
     
@@ -149,7 +149,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 1.0)
+        wait(for: [exp], timeout: 5.0)
         return receivedResult
     }
     
