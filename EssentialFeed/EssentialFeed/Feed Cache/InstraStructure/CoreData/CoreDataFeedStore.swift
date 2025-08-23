99f4513a5f61124c8b0d5bdcf4e2bc0826c38f0e
Marco Siccardi <msiccdev@hotmail.com>
2025-08-23 18:22:35 +0200

    Load NSManagedObjectModel instance lazily and cache it to prevent multiple `NSEntityDescriptions` claiming the same `NSManagedObject` model subclasses (This problem just generates warnings but could lead to undefined behavior in the future).

--- EssentialFeed/EssentialFeed/Feed Cache/InstraStructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/InstraStructure/CoreData/CoreDataFeedStore.swift
@@ -8,15 +8,28 @@
 import CoreData
 
 public final class CoreDataFeedStore {
-    private let container: NSPersistentContainer
+    private static let modelName = "FeedStore"
+    private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
     
+    private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
 
-    public init(storeURL: URL) throws {
-        let bundle = Bundle(for: CoreDataFeedStore.self)
+    enum StoreError: Error {
+        case modelNotFound
+        case failedToLoadPersistentContainer(Error)
+    }
 
-        container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
-        context = container.newBackgroundContext()
+    public init(storeURL: URL) throws {
+        guard let model = CoreDataFeedStore.model else {
+            throw StoreError.modelNotFound
+        }
+        
+        do {
+            container = try NSPersistentContainer.load(name: CoreDataFeedStore.modelName, model: model, url: storeURL)
+            context = container.newBackgroundContext()
+        } catch {
+            throw StoreError.failedToLoadPersistentContainer(error)
+        }
     }
 
     func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
@@ -30,7 +43,7 @@ public final class CoreDataFeedStore {
             try? coordinator.persistentStores.forEach(coordinator.remove)
         }
     }
-
+    
     deinit {
         cleanUpReferencesToPersistentStores()
     }
