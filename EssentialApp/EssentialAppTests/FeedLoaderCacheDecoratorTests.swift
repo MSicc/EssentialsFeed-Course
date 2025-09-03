f03b73ed97ee731863c87138652dbbdc3699a123
Marco Siccardi <msiccdev@hotmail.com>
2025-09-03 21:17:10 +0200

    Move FeedCache to the Feed Feature production group

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -11,11 +11,7 @@ import EssentialFeed
 import EssentialApp
 
 
-protocol FeedCache {
-    typealias Result = Swift.Result<Void, Error>
 
-    func save(_ feed: [FeedImage], completion: @escaping (Result) -> Void)
-}
 
 final class FeedLoaderCacheDecorator: FeedLoader {
     private let decoratee: FeedLoader
