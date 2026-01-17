cfa0fd63f3f5f5267e7122cdd7dee295c3fa281b
Marco Siccardi <msiccdev@hotmail.com>
2026-01-17 08:13:02 +0100

    Extract reusable FeedImageDataStoreSpecs

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedImageDataStoreSpecs/FeedImageDataStoreSpecs.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageDataStoreSpecs.swift
+//  EssentialFeedTests
+//
+//  Created by Marco Siccardi on 17.01.2026.
+//
+
+import Foundation
+
+protocol FeedImageDataStoreSpecs {
+    func test_retrieveImageData_deliversNotFoundWhenEmpty() throws
+    func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() throws
+    func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() throws
+    func test_retrieveImageData_deliversLastInsertedValue() throws
+}
